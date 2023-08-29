f1e971f7a714ca79e84505c80c8cb722fd061ba3
Matheus <matheusdeamorim20@gmail.com>
2023-08-29 10:25:46 -0300

    criando transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,28 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, response) => {
+    // { title, amount, type: credit or debit }
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    // HTTP Codes (201)
+    return response.status(201).send()
   })
 }
