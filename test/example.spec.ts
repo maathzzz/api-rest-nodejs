87446f2cc0c7302db78a6f6b3abc0d666e5d9742
Matheus <matheusdeamorim20@gmail.com>
2023-08-31 15:05:56 -0300

    categorias de testes

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,24 +1,27 @@
-import { expect, test, beforeAll, afterAll } from 'vitest'
+import { expect, test, beforeAll, afterAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('User can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  // test('User can create a new transaction', async () => {})
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
 
-  // vai verificar se o retorno é 201
+    // vai verificar se o retorno é 201
+  })
 })
