5413960db2d0f843eb7fd4471bc41ea4beb5508a
Matheus <matheusdeamorim20@gmail.com>
2023-08-31 15:54:37 -0300

    testando listagem

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, beforeAll, afterAll, describe, it } from 'vitest'
+import { expect, beforeAll, afterAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -21,7 +21,31 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
-
     // vai verificar se o retorno é 201
+  })
+
+  // => teste deve se excluir de todo e QUALQUER contexto
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.headers['set-cookie']
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transactions',
+        amount: 5000,
+      }),
+    ])
   })
 })
